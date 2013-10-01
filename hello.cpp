e32997d92967b7e42155c266afeb858201b95a78
lukkarhan <hanna.lukkarinen@hotmail.fi>
2013-10-01 16:21:56 +0300

    Hello World!

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+/* Kommentti alkaa
+ * ja jatkuu..
+ * ja jatkuu..
+ * ja loppuu. */
+// yhden rivin kommentti
+
+#include <iostream>
+using namespace std; 
+void main(void) // ohjelma alkaa t„st„
+{ // lohkosulku aloittaa p„„ohjelman
+	cout << "Hei \nmaailma! " << endl ;
+	cout << "Hyvin menee,\n"
+		<< "mutta menk””n!" << endl ;
+} // lohkosulku toiseen suuntaan lopettaa ohjelman
