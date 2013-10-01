098605e447ddb61f2a2e64778637514180924450
lukkarhan <hanna.lukkarinen@hotmail.fi>
2013-10-01 16:16:06 +0300

    Harjoitus 3 a, b
    Hello World, oman nimen ja osoitteen tulostaminen

---
+++ harj3a.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+void main (void)
+{
+cout << "Hanna Lukkarinen \nSienitie 4 A1\n40640 Jyv„skyl„" << endl;
+}
---
+++ harj3b.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+void main (void)
+{
+cout << "******************************\n* *\n* Hanna *\n* Lukkarinen *" <<endl
+	<< "* *\n*____________________________*\n* *\n* Sienitie 4 A1 *\n* 40640 Jyv„skyl„ *" <<endl
+	<< "* *\n******************************" <<endl ;
+}
